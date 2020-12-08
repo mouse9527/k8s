ce815dc2e9f5ce685573ea237ba89752ba8e966d
xh.wang <xh.wang@datsch.com.cn>
2020-12-08 14:43:59 +0800

    Refactored.

--- sh/kubeadm-install.sh
+++ sh/kubeadm-install.sh
@@ -5,7 +5,7 @@ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sudo sysctl --system
+sysctl --system
 
 cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
