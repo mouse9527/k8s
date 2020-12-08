eeecb232ad76d44a6f3cf0be47efc68f57959c71
xh.wang <xh.wang@datsch.com.cn>
2020-12-08 13:30:59 +0800

    Added docker-install.sh

---
+++ sh/docker-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+yum remove docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-engine
+yum install -y yum-utils
+
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
+systemctl enable docker
