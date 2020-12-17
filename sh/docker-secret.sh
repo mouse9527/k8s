da49b348446e7f05cce165a3586ddb090d180e62
xh.wang <xh.wang@datsch.com.cn>
2020-12-18 00:08:06 +0800

    rename

--- sh/docker-secret.sh
+++ sh/docker-secret.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-kubectl create secret docker-registry docker-secret \
+kubectl create secret docker-registry -n kube-system docker-secret \
   --docker-server=docker-repository.k8s.mouse.center:1443 \
   --docker-username=mouse \
   --docker-password=Hz970210
