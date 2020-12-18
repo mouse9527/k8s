a4f1803cedce4e0cbb57e854818b4a51fe83b020
xh.wang <xh.wang@datsch.com.cn>
2020-12-18 09:18:44 +0800

    removed docker-secret

--- sh/docker-secret.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl create secret docker-registry -n kube-system docker-secret \
-  --docker-server=docker-repository.k8s.mouse.center:1443 \
-  --docker-username=mouse \
-  --docker-password=Hz970210
