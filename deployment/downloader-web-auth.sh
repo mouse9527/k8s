67e8dc42fe65c933e9c95afed3d6625410175fb7
xh.wang <xh.wang@datsch.com.cn>
2020-12-24 11:05:17 +0800

    Fixed package

--- deployment/downloader-web-auth.sh
+++ deployment/downloader-web-auth.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-yum install -y htpasswd
+yum install -y httpd-tools
 htpasswd -c downloader-web-auth downloader
 kubectl create secret generic basic-auth --from-file=downloader-web-auth
