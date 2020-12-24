7a6acbcdac0564bebf0699eff8416db2dffee1e3
xh.wang <xh.wang@datsch.com.cn>
2020-12-24 11:44:38 +0800

    Fixed auth

--- deployment/downloader-web-auth.sh
+++ deployment/downloader-web-auth.sh
@@ -2,4 +2,4 @@
 set -e
 yum install -y httpd-tools
 htpasswd -c downloader-web-auth downloader
-kubectl create secret -n app generic basic-auth --from-file=downloader-web-auth
+kubectl create secret -n app generic downloader-web-auth --from-file=downloader-web-auth
