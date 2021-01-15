0748ae58029dd532f3b69085e4c2b8fd53be92ec
xh.wang <xh.wang@datsch.com.cn>
2021-01-15 17:40:05 +0800

    Fixed version

--- deployment/cd/gocd-image.sh
+++ deployment/cd/gocd-image.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 docker build -t docker-push.mouse.center:1443/gocd-agent-java14-node-docker - <./Dockerfile-gocd-agent
 docker push docker-push.mouse.center:1443/gocd-agent-java14-node-docker
+docker build -t docker-push.mouse.center:1443/gocd-server:v19.12.0 - < ./Dockerfile-gocd-server
+docker push docker-push.mouse.center:1443/gocd-server:v19.12.0
