ee36c3ab46f178ff9f7ba989f26e1357869f33cf
xh.wang <xh.wang@datsch.com.cn>
2021-01-26 12:34:18 +0800

    added namespace

--- deployment/cd/gocd-agent-kubectl-secret.sh
+++ deployment/cd/gocd-agent-kubectl-secret.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl create secret generic gocd-agent-kubectl-secret --from-file=config=/etc/kubernetes/admin.conf
+kubectl create secret -n app generic gocd-agent-kubectl-secret --from-file=config=/etc/kubernetes/admin.conf
