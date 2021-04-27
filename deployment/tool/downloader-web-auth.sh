7d901c0477eb459540a54c64ca1eac30379b64f8
mouse9527 <m1689141525@gmail.com>
2021-04-27 14:52:27 +0800

    update

--- deployment/tool/downloader-web-auth.sh
+++ deployment/tool/downloader-web-auth.sh
@@ -2,4 +2,4 @@
 set -e
 yum install -y httpd-tools
 htpasswd -c auth root
-kubectl create secret -n app generic downloader-web-auth --from-file=auth
+kubectl create secret -n tool generic downloader-web-auth --from-file=auth
