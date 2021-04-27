22e8f4aeb1e0a931078c92603e4823d6c15f3b5f
mouse9527 <m1689141525@gmail.com>
2021-04-27 14:36:54 +0800

    update

--- sh/init.sh
+++ sh/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubeadm init --pod-network-cidr "10.244.0.0/16" --image-repository "docker-repository.mouse.center:1443"
+kubeadm init --pod-network-cidr "10.244.0.0/16" --image-repository "repository.mouse.center:1443"
 echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> ~/.bash_profile
 source ~/.bash_profile
 
--- sh/kubeadm-install.sh
+++ sh/kubeadm-install.sh
@@ -10,7 +10,7 @@ sysctl --system
 cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
