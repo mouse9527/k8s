928e7456c59ba1abb4dd977679728be9f2ce9d5b
mouse9527 <m1689141525@gmail.com>
2021-04-28 13:46:11 +0800

    update

--- deployment/db/replication.js
+++ deployment/db/replication.js
@@ -1,8 +1,8 @@
 rs.initiate({
     _id: "rs0",
     members: [
-        {_id: 0, host: "mongo-replication-0.mongo-replication.db:27017"},
-        {_id: 1, host: "mongo-replication-1.mongo-replication.db:27017"},
-        {_id: 2, host: "mongo-replication-2.mongo-replication.db:27017"}
+        {_id: 0, host: "mongo-0.mongo.db:27017"},
+        {_id: 1, host: "mongo-1.mongo.db:27017"},
+        {_id: 2, host: "mongo-2.mongo.db:27017"}
     ]
 })
