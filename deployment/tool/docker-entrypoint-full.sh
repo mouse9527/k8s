de1afc2cf8ea00919ff831eba331e7335610adba
mouse9527 <m1689141525@gmail.com>
2021-05-14 22:08:34 +0800

    update

--- deployment/tool/docker-entrypoint-full.sh
+++ deployment/tool/docker-entrypoint-full.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 
-supervisord -c /supervisord.conf &
-
 php -f /var/www/html/occ fulltextsearch:live  &
 php -f /var/www/html/occ files_external:notify 6 &
 php -f /var/www/html/occ files_external:notify 7 &
