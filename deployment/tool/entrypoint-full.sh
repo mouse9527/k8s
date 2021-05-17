e9ea989a98992e2615221c824ae2b519efc23587
mouse9527 <m1689141525@gmail.com>
2021-05-17 13:48:19 +0800

    update

--- deployment/tool/entrypoint-full.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "fulltextsearch:live"
-php -f /var/www/html/occ files_external:notify 6 -v > /var/log/notify-6.log  &
-echo "files_external:notify 6"
-php -f /var/www/html/occ files_external:notify 7 -v > /var/log/notify-7.log  &
-echo "files_external:notify 7"
-php -f /var/www/html/occ files_external:notify 8 -v > /var/log/notify-8.log  &
-echo "files_external:notify 8"
