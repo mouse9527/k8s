b4e6df7f464a77c40eab23cd5a7f7b4296d45d12
mouse <m1689141525@gmail.com>
2021-08-02 22:26:19 +0800

    refactor.

---
+++ deployment/app/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -e ~/linuxgsm.sh ]; then
+    echo "Initializing Linuxgsm User Script in New Volume"
+    cp /linuxgsm.sh ./linuxgsm.sh
+fi
+
+# with no command, just spawn a running container suitable for exec's
+if [ $# = 0 ]; then
+    tail -f /dev/null
+else
+    # execute the command passed through docker
+    "$@"
+
+    # if this command was a server start cmd
+    # to get around LinuxGSM running everything in
+    # tmux;
+    # we attempt to attach to tmux to track the server
+    # this keeps the container running
+    # when invoked via docker run
+    # but requires -it or at least -t
+    tmux set -g status off && tmux attach 2> /dev/null
+fi
+
+exec "$@"
