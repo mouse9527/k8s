8a29ec4b3cca8587937bdcae7f7e50c543ee5d2b
mouse <m1689141525@gmail.com>
2021-08-04 18:56:07 +0800

    update

--- deployment/app/entrypoint.sh
+++ deployment/app/entrypoint.sh
@@ -8,30 +8,4 @@ else
     cp /linuxgsm.sh /app/linuxgsm.sh
 fi
 
-if [ ! -e /app/arkserver ];
-then
-    echo "No arkserver!"
-    return -1
-else
-    echo "Starting game server"
-    bash /app/start.sh
-fi
-
-# with no command, just spawn a running container suitable for exec's
-if [ $# = 0 ]; then
-    tail -f /dev/null
-else
-    # execute the command passed through docker
-    "$@"
-
-    # if this command was a server start cmd
-    # to get around LinuxGSM running everything in
-    # tmux;
-    # we attempt to attach to tmux to track the server
-    # this keeps the container running
-    # when invoked via docker run
-    # but requires -it or at least -t
-    tmux set -g status off && tmux attach 2> /dev/null
-fi
-
-exec "$@"
+bash /app/start.sh
