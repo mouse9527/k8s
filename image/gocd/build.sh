e6af717c948eb4d3bc6a0ad6b343ce73e14fbd94
mouse <m1689141525@gmail.com>
2021-10-29 10:40:26 +0800

    refactor.

--- image/gocd/build.sh
+++ image/gocd/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-docker build -t registry.mouse.center:1443/gocd-agent-java-node-docker:java17_gradle7.2 -f ./Dockerfile-gocd-agent .
-docker push registry.mouse.center:1443/gocd-agent-java-node-docker:java17_gradle7.2
+VERSION=v21.3.0
 
-docker build -t registry.mouse.center:1443/gocd-server:v21.1.0 -f ./Dockerfile-gocd-server .
-docker push registry.mouse.center:1443/gocd-server:v21.1.0
+docker build -t registry.mouse.center:1443/gocd-agent-java17:$VERSION -f ./Dockerfile-gocd-agent .
+docker push registry.mouse.center:1443/gocd-agent-java17:$VERSION
 
-docker build -t registry.mouse.center:1443/gocd-agent-kubectl -f ./Dockerfile-gocd-agent-kubectl .
-docker push registry.mouse.center:1443/gocd-agent-kubectl
+docker build -t registry.mouse.center:1443/gocd-server:$VERSION -f ./Dockerfile-gocd-server .
+docker push registry.mouse.center:1443/gocd-server:$VERSION
+
+docker build -t registry.mouse.center:1443/gocd-agent-kubectl:$VERSION -f ./Dockerfile-gocd-agent-kubectl .
+docker push registry.mouse.center:1443/gocd-agent-kubectl:$VERSION
