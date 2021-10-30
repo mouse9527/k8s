ce8ef0259c2b32351d108813e95e8712f0a795a9
mouse <m1689141525@gmail.com>
2021-10-31 01:15:28 +0800

    update config

--- image/dev/build.sh
+++ image/dev/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
- docker build -t registry.mouse.center:1443/glowroot-central:0.14.0-beta -f ./Dockerfile-glowroot-central .
- docker push registry.mouse.center:1443/glowroot-central:0.14.0-beta
+docker build -t registry.mouse.center:1443/glowroot-central:0.14.0-beta -f ./Dockerfile-glowroot-central .
+docker push registry.mouse.center:1443/glowroot-central:0.14.0-beta
