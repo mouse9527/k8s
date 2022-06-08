c2cf04f6eacc1f66c99577a6b73b3ef5d457e532
mouse <m1689141525@gmail.com>
2022-06-09 00:59:52 +0800

    refacgor.

--- image/gocd/build.sh
+++ image/gocd/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 VERSION=v21.3.0
 
-docker build -t registry.mouse.center:1443/gocd-agent-java17:$VERSION -f ./Dockerfile-gocd-agent .
+docker build -t registry.mouse.center:1443/gocd-agent-java17:$VERSION -f ./Dockerfile-gocd-agent-java17 .
 docker push registry.mouse.center:1443/gocd-agent-java17:$VERSION
+
+docker build -t registry.mouse.center:1443/gocd-agent-node:$VERSION -f ./Dockerfile-gocd-agent-node .
+docker push registry.mouse.center:1443/gocd-agent-node:$VERSION
 
 docker build -t registry.mouse.center:1443/gocd-server:$VERSION -f ./Dockerfile-gocd-server .
 docker push registry.mouse.center:1443/gocd-server:$VERSION
