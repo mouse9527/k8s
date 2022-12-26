ffa901a0d07f318b5b31f907f1223bbce60bb675
mouse <m1689141525@gmail.com>
2022-12-26 20:54:09 +0800

    update

--- image/app/klipper.sh
+++ image/app/klipper.sh
@@ -1,2 +1,2 @@
-docker build -build-arg https_proxy=http://win10:7890 -build-arg http_proxy=http://win10:7890 -build-arg all_proxy=socks5://win10:7890 -t registry.mouse.center:1443/klipper-moonraker:20221225-1347 -f ./Dockerfile-klipper-moonraker .
+docker build -t registry.mouse.center:1443/klipper-moonraker:20221225-1347 -f ./Dockerfile-klipper-moonraker .
 docker push registry.mouse.center:1443/klipper-moonraker:20221225-1347
