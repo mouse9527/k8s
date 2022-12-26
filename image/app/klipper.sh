2bc5ddb32630fa7e580d3c29149c9f6176fa0a6d
mouse <m1689141525@gmail.com>
2022-12-26 20:57:55 +0800

    update

--- image/app/klipper.sh
+++ image/app/klipper.sh
@@ -1,2 +1,2 @@
-docker build -t registry.mouse.center:1443/klipper-moonraker:20221225-1347 -f ./Dockerfile-klipper-moonraker .
+docker build --no-cache -t registry.mouse.center:1443/klipper-moonraker:20221225-1347 -f ./Dockerfile-klipper-moonraker .
 docker push registry.mouse.center:1443/klipper-moonraker:20221225-1347
