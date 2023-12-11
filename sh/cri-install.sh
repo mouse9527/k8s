0d101921a089b604931456c433b82c5a524d5c4e
mouse <m1689141525@gmail.com>
2023-12-11 17:12:45 +0800

    update

--- sh/cri-install.sh
+++ sh/cri-install.sh
@@ -7,7 +7,11 @@ yum remove docker \
   docker-latest \
   docker-latest-logrotate \
   docker-logrotate \
-  docker-engine
+  docker-engine \
+  cri-docker.socket
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
 yum install -y yum-utils
 
 yum-config-manager \
@@ -15,13 +19,14 @@ yum-config-manager \
   https://download.docker.com/linux/centos/docker-ce.repo
 
 yum install -y docker-ce docker-ce-cli containerd.io
+containerd config default > /etc/containerd/config.toml
 
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.8/cri-dockerd-0.3.8-3.el7.x86_64.rpm
-
-rpm -ivh cri-dockerd-0.3.8-3.el7.x86_64.rpm
-
-systemctl daemon-reload
-systemctl enable --now cri-docker.socket
+#wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.8/cri-dockerd-0.3.8-3.el7.x86_64.rpm
+#
+#rpm -ivh cri-dockerd-0.3.8-3.el7.x86_64.rpm
+#
+#systemctl daemon-reload
+#systemctl enable --now cri-docker.socket
 
 systemctl start docker
 systemctl enable docker
