e2e28d197b4844c4402b61957a138a8546265b42
mouse <m1689141525@gmail.com>
2023-12-11 16:57:57 +0800

    add cri-docker

--- sh/kubeadm-install.sh
+++ sh/kubeadm-install.sh
@@ -3,11 +3,23 @@ set -e
 
 yum remove kubelet kubeadm kubectl -y
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
 EOF
-sysctl --system
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# 设置所需的 sysctl 参数，参数在重新启动后保持不变
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# 应用 sysctl 参数而不重新启动
+sudo sysctl --system
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
